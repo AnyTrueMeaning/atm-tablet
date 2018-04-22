751ffc6c0b89592ab2a8295b53e7d188bd6f1fcf
Daniel Hengeveld <danielwh@gmail.com>
2018-04-22 23:46:37 +0200

    add a bunch of placeholder components

---
+++ components/CallToAction.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import CommonStyles from '../CommonStyles'
+
+class CallToAction extends Component {
+  render () {
+    return (
+      <View style={CommonStyles.container}>
+
+      </View>
+    )
+  }
+}
+
+export default CallToAction
---
+++ components/FirstFlower.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import CommonStyles from '../CommonStyles'
+
+class FirstFlower extends Component {
+  render () {
+    return (
+      <View style={CommonStyles.container}>
+
+      </View>
+    )
+  }
+}
+
+export default FirstFlower
